5d59d05afc84b0ed1f1bfa3de4780b25e3f82963
Kene David Nwosu <kendavidn@gmail.com>
2022-11-04 16:28:21 +0100

    restructure autograders

--- untangled_ls02_filter/autograder/ls02_filter_autograder.R
+++ untangled_ls02_filter/autograder/ls02_filter_autograder.R
@@ -20,284 +20,359 @@ pacman::p_load(praise)
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 .scores <- rep(-1, times = 9)   # Put total number of questions as `times` argument
 
-.NUM_Q_weight_to_g <- 1
-.NUM_Q_sarcopenia_resp_id <- 2
-.NUM_Q_sarcopenia_grip_strength <- 3
-.NUM_Q_women_low_grip_strength <- 4
-.NUM_Q_prop_women_low_grip_strength <- 5
-.NUM_Q_asm_calculation <- 6
-.NUM_Q_age_integer <- 7
-.NUM_Q_age_integer <- 8
-.NUM_Q_age_integer <- 9
+.NUM_Q_is_pregnant <- 1
+.NUM_Q_female_nrow <- 2
+.NUM_Q_under_18 <- 3
+.NUM_Q_tsinga_messa<- 4
+.NUM_Q_male_positive <- 5
+.NUM_Q_child_primary <- 6
+.NUM_Q_not_tsinga_messa <- 7
+.NUM_Q_na_smoker <- 8
+.NUM_Q_drop_resp_under_20 <- 9
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~  q1 ----
+## ~  Q_is_pregnant ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.CHECK_Q_1 <-
+.CHECK_Q_is_pregnant <-
   function() {
-    .problem_number <<- 1
+    .problem_number <<- .NUM_Q_is_pregnant
     
-    .q1 <- .yao %>% filter(is_pregnant == "Yes")
+    .Q_is_pregnant <- .yao %>% filter(is_pregnant == "Yes")
     
     .autograder <<- 
       function(){
-        if (!is.data.frame(q1)) return(c(value = -1, message = "Your result should be a dataframe."))
+        if (!is.data.frame(Q_is_pregnant)) return(c(value = -1, message = "Your result should be a dataframe."))
         
-        if (isTRUE(all_equal(q1, .q1))) return(c(value = 1, message = paste("Correct!", praise())))
+        if (isTRUE(all_equal(Q_is_pregnant, .Q_is_pregnant))) return(c(value = 1, message = paste("Correct!", praise())))
         
         return(c(value = 0, message = "Wrong. Please try again."))
       }
     .apply_autograder()
   }
 
-.HINT_Q_1 <- function(){ 
-  'YOURDATA %>% filter(CONDITION)' -> out
+.HINT_Q_is_pregnant <- function(){ 
+  'HINT: 
+  YOURDATA %>% filter(CONDITION)' -> out
   cat(out)
 }
 
+.SOLUTION_Q_is_pregnant <- function(){ 
+  "SOLUTION: 
+  yao %>% filter(is_pregnant == 'Yes')" -> out
+  cat(out)
+}
+
+
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~  q2 ----
+## ~  Q_female_nrow ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.CHECK_Q_2 <-
+.CHECK_Q_female_nrow <-
   function() {
-    .problem_number <<- 2
+    .problem_number <<- .NUM_Q_female_nrow
     
-    .q2 <- .yao %>%
+    .Q_female_nrow <- .yao %>%
       filter(sex == "Female") %>%
       nrow()
     
     .autograder <<- 
       function(){
-        if (!is.numeric(q2)) return(c(value = -1, message = "Your result should be numeric."))
+        if (!is.numeric(Q_female_nrow)) return(c(value = -1, message = "Your result should be numeric."))
         
-        if (identical(q2, .q2)) return(c(value = 1, message = paste("Correct!", praise())))
+        if (identical(Q_female_nrow, .Q_female_nrow)) return(c(value = 1, message = paste("Correct!", praise())))
         
         return(c(value = 0, message = "Wrong. Please try again."))
       }
     .apply_autograder()
   }
 
-.HINT_Q_2 <- function(){ 
-'YOURDATA %>% 
+.HINT_Q_female_nrow <- function(){ 
+'HINT: 
+  YOURDATA %>% 
   filter(sex == "YOURSTRING") %>%
   nrow()' -> out
   cat(out)
 }
 
+.SOLUTION_Q_is_pregnant <- function(){ 
+  "SOLUTION: 
+      yao %>%
+      filter(sex == 'Female') %>%
+      nrow()" -> out
+  cat(out)
+}
+
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~  q3 ----
+## ~  Q_under_18 ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.CHECK_Q_3 <-
+.CHECK_Q_under_18 <-
   function() {
-    .problem_number <<- 3
+    .problem_number <<- .NUM_Q_under_18
     
-    .q3 <- .yao %>%
-      filter(age < 18)
+    .Q_under_18 <- .yao %>% filter(age < 18)
     
     .autograder <<- 
       function(){
-        if (!is.data.frame(q3)) return(c(value = -1, message = "Your result should be a data frame."))
+        if (!is.data.frame(Q_under_18)) return(c(value = -1, message = "Your result should be a data frame."))
         
-        if (isTRUE(all_equal(q3, .q3))) return(c(value = 1, message = paste("Correct!", praise())))
+        if (isTRUE(all_equal(Q_under_18, .Q_under_18))) return(c(value = 1, message = paste("Correct!", praise())))
         
         return(c(value = 0, message = "Wrong. Please try again."))
       }
     .apply_autograder()
   }
 
-.HINT_Q_3 <- function(){ 
+.HINT_Q_under_18 <- function(){ 
 'YOURDATA %>% 
   filter(CONDITION)' -> out
   cat(out)
 }
 
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~  q4 ----
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.CHECK_Q_4 <-
-  function() {
-    .problem_number <<- 4
-    
-    .q4 <- .yao %>%
-      filter(neighborhood %in% c("Tsinga", "Messa") ) 
-    
-    .autograder <<- 
-      function(){
-        if (!is.data.frame(q4)) return(c(value = -1, message = "Your result should be a data frame."))
-        
-        if (isTRUE(all_equal(q4, .q4))) return(c(value = 1, message = paste("Correct!", praise())))
-        
-        return(c(value = 0, message = "Wrong. Please try again."))
-      }
-    .apply_autograder()
-  }
-
-.HINT_Q_4 <- function(){ 
-'YOURDATA %>% 
-  filter(neighborhood %in% c("STRING1", "STRING2"))' -> out
+.SOLUTION_Q_under_18 <- function(){ 
+  "SOLUTION: 
+  yao %>% filter(age < 18)" -> out
   cat(out)
 }
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~  q5 ----
+## ~  Q_tsinga_messa ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.CHECK_Q_5 <-
+.CHECK_Q_tsinga_messa <-
   function() {
-    .problem_number <<- 5
+    .problem_number <<- .NUM_Q_tsinga_messa
     
-    .q5 <- .yao %>%
-      filter(sex == "Male" & igg_result == "Positive") 
+    .Q_tsinga_messa <- .yao %>%
+      filter(neighborhood %in% c("Tsinga", "Messa")) 
     
     .autograder <<- 
       function(){
-        if (!is.data.frame(q5)) return(c(value = -1, message = "Your result should be a data frame."))
+        if (!is.data.frame(Q_tsinga_messa)) return(c(value = -1, message = "Your result should be a data frame."))
         
-        if (isTRUE(all_equal(q5, .q5))) return(c(value = 1, message = paste("Correct!", praise())))
+        if (isTRUE(all_equal(Q_tsinga_messa, .Q_tsinga_messa))) return(c(value = 1, message = paste("Correct!", praise())))
         
         return(c(value = 0, message = "Wrong. Please try again."))
       }
     .apply_autograder()
   }
 
-.HINT_Q_5 <- function(){ 
-'YOURDATA %>% 
+.HINT_Q_tsinga_messa <- function(){ 
+'HINT: 
+  YOURDATA %>% 
+  filter(neighborhood %in% c("STRING1", "STRING2"))' -> out
+  cat(out)
+}
+
+.SOLUTION_Q_tsinga_messa <- function(){ 
+  'SOLUTION: 
+  yao %>%
+  filter(neighborhood %in% c("Tsinga", "Messa"))' -> out
+  cat(out)
+}
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## ~  Q_male_positive ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+.CHECK_Q_male_positive <-
+  function() {
+    .problem_number <<- .NUM_Q_male_positive
+    
+    .Q_male_positive <- .yao %>%
+      filter(sex == "Male" & igg_result == "Positive") 
+    
+    .autograder <<- 
+      function(){
+        if (!is.data.frame(Q_male_positive)) return(c(value = -1, message = "Your result should be a data frame."))
+        
+        if (isTRUE(all_equal(Q_male_positive, .Q_male_positive))) return(c(value = 1, message = paste("Correct!", praise())))
+        
+        return(c(value = 0, message = "Wrong. Please try again."))
+      }
+    .apply_autograder()
+  }
+
+.HINT_Q_male_positive <- function(){ 
+'HINT: 
+  YOURDATA %>% 
   filter(sex == "Male" & CONDITION2) %>%
   nrow()' -> out
   cat(out)
 }
 
+.SOLUTION_Q_male_positive <- function(){ 
+  'SOLUTION: 
+  yao %>%
+  filter(sex == "Male" & igg_result == "Positive")' -> out
+  cat(out)
+}
+
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~  q6 ----
+## ~  Q_child_primary ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.CHECK_Q_6 <-
+.CHECK_Q_child_primary <-
   function() {
-    .problem_number <<- 6
+    .problem_number <<- .NUM_Q_child_primary
     
-    .q6 <- .yao %>% filter(age < 18 | highest_education == "Primary")
+    .Q_child_primary <- .yao %>% filter(age < 18 | highest_education == "Primary")
     
-    .q6_wrong_operator <- .yao %>% filter(age < 18 & highest_education == "Primary")
+    .Q_child_primary_wrong_operator <- .yao %>% filter(age < 18 & highest_education == "Primary")
     
     .autograder <<- 
       function(){
-        if (!is.data.frame(q6)) return(c(value = -1, message = "Your result should be a data frame."))
+        if (!is.data.frame(Q_child_primary)) return(c(value = -1, message = "Your result should be a data frame."))
         
-        if (isTRUE(all_equal(q6, .q6_wrong_operator))) return(c(value = 0, message = paste("Wrong. You should use the `|` operator.",
+        if (isTRUE(all_equal(Q_child_primary, .Q_child_primary_wrong_operator))) return(c(value = 0, message = paste("Wrong. You should use the `|` operator.",
                                                                                 "We actually want children OR anyone with a 'Primary' highest_education",
-                                                                                "(despite it saying 'and' in the question).")))
+                                                                                "(despite it saying 'and' in the question). Think for some time about this. 
+                                                                                It is quite counterintuitive!")))
         
-        if (isTRUE(all_equal(q6, .q6))) return(c(value = 1, message = paste("Correct!", praise())))
+        if (isTRUE(all_equal(Q_child_primary, .Q_child_primary))) return(c(value = 1, message = paste("Correct!", praise())))
         
         return(c(value = 0, message = "Wrong. Please try again."))
       }
     .apply_autograder()
   }
 
-.HINT_Q_6 <- function(){ 
-  'YOURDATA %>% filter(CONDITION1 | CONDITION2)' -> out
+.HINT_Q_child_primary <- function(){ 
+  'HINT: 
+  YOURDATA %>% filter(CONDITION1 | CONDITION2)' -> out
+  cat(out)
+}
+
+.SOLUTION_Q_child_primary <- function(){ 
+  'SOLUTION: 
+  yao %>% filter(age < 18 | highest_education == "Primary")' -> out
   cat(out)
 }
 
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~  q7 ----
+## ~  Q_not_tsinga_messa ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.CHECK_Q_7 <-
+.CHECK_Q_not_tsinga_messa <-
   function() {
-    .problem_number <<- 7
+    .problem_number <<- .NUM_Q_not_tsinga_messa
     
-    .q7 <- .yao %>%
+    .Q_not_tsinga_messa <- .yao %>%
       filter(!(neighborhood %in% c("Tsinga", "Messa"))) 
     
     .autograder <<- 
       function(){
-        if (!is.data.frame(q7)) return(c(value = -1, message = "Your result should be dataframe."))
+        if (!is.data.frame(Q_not_tsinga_messa)) return(c(value = -1, message = "Your result should be dataframe."))
         
-        if (isTRUE(all_equal(q7, .q7))) return(c(value = 1, message = paste("Correct!", praise())))
+        if (isTRUE(all_equal(Q_not_tsinga_messa, .Q_not_tsinga_messa))) return(c(value = 1, message = paste("Correct!", praise())))
         
         return(c(value = 0, message = "Wrong. Please try again."))
       }
     .apply_autograder()
   }
 
-.HINT_Q_7 <- function(){ 
-'YOURDATA %>% 
+.HINT_Q_not_tsinga_messa <- function(){ 
+'HINT: 
+  YOURDATA %>% 
   filter(neighborhood %in% c("STRING1", "STRING2") )' -> out
   cat(out)
 }
 
+.SOLUTION_Q_not_tsinga_messa <- function(){ 
+  'SOLUTION: 
+  yao %>%
+  filter(!(neighborhood %in% c("Tsinga", "Messa")))' -> out
+  cat(out)
+}
+
+
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~  q8 ----
+## ~  Q_na_smoker ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.CHECK_Q_8 <-
+.CHECK_Q_na_smoker <-
   function() {
-    .problem_number <<- 8
+    .problem_number <<- .NUM_Q_na_smoker
     
-    .q8 <- .yao %>% filter(is.na(is_smoker)) 
+    .Q_na_smoker <- .yao %>% filter(is.na(is_smoker)) 
     
     .autograder <<- 
       function(){
-        if (!is.data.frame(q8)) return(c(value = -1, message = "Your result should be dataframe."))
+        if (!is.data.frame(Q_na_smoker)) return(c(value = -1, message = "Your result should be dataframe."))
         
-        if (isTRUE(all_equal(q8, .q8))) return(c(value = 1, message = paste("Correct!", praise())))
+        if (isTRUE(all_equal(Q_na_smoker, .Q_na_smoker))) return(c(value = 1, message = paste("Correct!", praise())))
         
         return(c(value = 0, message = "Wrong. Please try again."))
       }
     .apply_autograder()
   }
 
-.HINT_Q_8 <- function(){ 
-'YOURDATA %>% 
+.HINT_Q_na_smoker <- function(){ 
+'HINT: 
+  YOURDATA %>% 
   filter(is.na(VARIABLE))' -> out
   cat(out)
 }
 
+.SOLUTION_Q_na_smoker <- function(){ 
+  "SOLUTION: 
+  yaounde %>% select(!c(highest_education:consultation))" -> out
+  cat(out)
+}
+
+
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~  q9 ----
+## ~  Q_drop_resp_under_20 ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.CHECK_Q_9 <-
+.CHECK_Q_drop_resp_under_20 <-
   function() {
-    .problem_number <<- 9
+    .problem_number <<- .NUM_Q_drop_resp_under_20
     
-    .q9 <- .yaounde %>% filter(respiration_frequency >= 20 | is.na(respiration_frequency))
+    .Q_drop_resp_under_20 <- .yaounde %>% filter(respiration_frequency >= 20 | is.na(respiration_frequency))
     
-    .q9_wrong_forgot_na <- .yaounde %>% filter(respiration_frequency > 20)
+    .Q_drop_resp_under_20_wrong_forgot_na <- .yaounde %>% filter(respiration_frequency > 20)
     
     .autograder <<- 
       function(){
-        if (!is.data.frame(q9)) return(c(value = -1, message = "Your result should be a data frame."))
+        if (!is.data.frame(Q_drop_resp_under_20))
+          .na("Your result should be a data frame.")
+        if (isTRUE(all_equal(Q_drop_resp_under_20, .Q_drop_resp_under_20_wrong_forgot_na))) 
+          .fail("Remember to to also keep those with NA values.")
         
-        if (isTRUE(all_equal(q9, .q9_wrong_forgot_na))) return(c(value = 0, message = "Remember to to also keep those with NA values."))
+        if (isTRUE(all_equal(Q_drop_resp_under_20, .Q_drop_resp_under_20))) 
+          .pass()
         
-        if (isTRUE(all_equal(q9, .q9))) return(c(value = 1, message = paste("Correct!", praise())))
-        
+        else
+          .fail()
         return(c(value = 0, message = "Wrong. Please try again."))
       }
     .apply_autograder()
   }
 
-.HINT_Q_9 <- function(){ 
-  'YOURDATA %>% filter(CONDITION1 | NA_CONDITION )' -> out
+.HINT_Q_drop_resp_under_20 <- function(){ 
+  'HINT: 
+  YOURDATA %>% filter(CONDITION1 | NA_CONDITION )' -> out
+  cat(out)
+}
+
+.SOLUTION_Q_drop_resp_under_20 <- function(){ 
+  "SOLUTION: 
+  yaounde %>% filter(respiration_frequency >= 20 | is.na(respiration_frequency))" -> out
   cat(out)
 }
 
 
 # 
 # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ## ~  q10 ----
+# ## ~  Q_is_pregnant0 ----
 # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # .CHECK_Q_10 <-
 #   function() {
 #     .problem_number <<- 10
 #     
-#     .q10 <- .yao %>% filter(row_number() %in% c(8:20, 80))
+#     .Q_is_pregnant0 <- .yao %>% filter(row_number() %in% c(8:20, 80))
 #     
 #     .autograder <<- 
 #       function(){
-#         if (!is.data.frame(q10)) return(c(value = -1, message = "Your result should be a data frame."))
+#         if (!is.data.frame(Q_is_pregnant0)) return(c(value = -1, message = "Your result should be a data frame."))
 #         
-#         if (isTRUE(all_equal(q10, .q10))) return(c(value = 1, message = paste("Correct!", praise())))
+#         if (isTRUE(all_equal(Q_is_pregnant0, .Q_is_pregnant0))) return(c(value = 1, message = paste("Correct!", praise())))
 #         
 #         return(c(value = 0, message = "Wrong. Please try again."))
 #       }
@@ -311,37 +386,3 @@ pacman::p_load(praise)
 # 
 # 
 # 
-# ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ## ~  q11 ----
-# ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# .CHECK_Q_11 <-
-#   function() {
-#     .problem_number <<- 11
-#     
-#     .q11 <- .yao %>% filter(str_detect(occupation, "Student"))
-#     
-#     .q11_wrong_used_equality_test <- .yao %>% filter(occupation == "Student")
-#     
-#     .autograder <<- 
-#       function(){
-#         if (!is.data.frame(q11)) return(c(value = -1, message = "Your result should be a data frame."))
-#         
-#         if (isTRUE(all_equal(q11, .q11_wrong_used_equality_test))) return(c(value = 0, message = cat("Wrong.",
-#                                                                                                     "It seems you tested for equality with `== 'Student'`", 
-#                                                                                                     "That will keep people who are ONLY students.",
-#                                                                                                     "You want to include students who have other occupations.")))
-#         
-#         if (isTRUE(all_equal(q11, .q11))) return(c(value = 1, message = paste("Correct!", praise())))
-#         
-#         return(c(value = 0, message = "Wrong. Please try again."))
-#       }
-#     .apply_autograder()
-#   }
-# 
-# 
-# .HINT_Q_11 <- function(){ 
-#   'YOURDATA %>% filter(str_detect(COLUMN, PATTERN))' -> out
-#   cat(out)
-# }
-
-
